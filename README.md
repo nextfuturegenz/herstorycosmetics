ca924b5cf9f9ee24b5970a98d79747f073b3a3a4
Ashish <ceo@nextfuturegenz.com>
2023-08-27 20:16:38 +0530

    feat: add base structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Shopping Cart
+
+herstory application built with React and Firebase. Currently WIP.
+
+## Features
+
+- Products Display.
+- Products Filter.
+- Infinite Scroll.
+- Cart Functions.
+- Basic Inventory Management.
+- Auth (Email/Password & Anonymous).
+- User orders & addresses.
---
+++ src/db/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyByL6xurKd5urmW5U1d2OkW_GsHOZ54stw",
+    authDomain: "herstory-cosmertics.firebaseapp.com",
+    projectId: "herstory-cosmertics",
+    storageBucket: "herstory-cosmertics.appspot.com",
+    messagingSenderId: "1017660077581",
+    appId: "1:1017660077581:web:cdf85d2f96826d238a4744",
+    measurementId: "G-32W9HRYKP0"
+};
